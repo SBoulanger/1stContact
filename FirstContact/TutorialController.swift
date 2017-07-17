c478418df63895c3d3e117068e960506c5c918d9
Samuel Boulanger <samuelboulanger1@gmail.com>
2017-07-16 19:53:35 -0700

    Added screenshots and tutorial basics

---
+++ FirstContact/TutorialController.swift
@@ -0,0 +1,11 @@
+//
+//  TutorialController.swift
+//  FirstContact
+//
+//  Created by Samuel Boulanger on 7/15/17.
+//  Copyright © 2017 Samuel Boulanger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
